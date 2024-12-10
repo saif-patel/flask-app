f5d03c58dc8b3a665ea7c9a5fb6eaa5ea83dd128
saif-patel <patelsaif29@gmail.com>
2024-12-10 00:59:52 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from saif patel ECS Container."
+    return "Hello from Saif Patel ECS Container, Updated"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
